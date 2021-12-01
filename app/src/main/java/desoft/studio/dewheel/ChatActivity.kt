e7f1ab5e0c7373d56281d6eca317eba246603867
Kitty <jkgraphicdesign18@gmail.com>
2021-11-30 21:15:58 -0800

    finally collapse, to have the eefect, toolbar height and collapsing toolbar height must be difference. Will testing more in the future

--- app/src/main/java/desoft/studio/dewheel/ChatActivity.kt
+++ app/src/main/java/desoft/studio/dewheel/ChatActivity.kt
@@ -3,6 +3,7 @@ package desoft.studio.dewheel
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import desoft.studio.dewheel.kata.WheelJolly
 
 class ChatActivity : AppCompatActivity() {
@@ -12,7 +13,8 @@ class ChatActivity : AppCompatActivity() {
 * *     ONCREATE
  * SET UP DATA FROM BUNDLE
 */
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat);
 
@@ -21,20 +23,41 @@ class ChatActivity : AppCompatActivity() {
         if(bdl != null) {
             Log.d(TAG, "onCreate: ${bdl.getParcelable<WheelJolly>(chatJollykey)}");
         }
+        // _ setup view
+        KF_SETUP_VIEWS();
+    }
 
+    /**
+    * * KF_SETUP_VIEWS
+    */
+    private fun KF_SETUP_VIEWS()
+    {
+        var tblayout = (findViewById<CollapsingToolbarLayout>(R.id.chat_collapse_tb));
+        //tblayout.isTitleEnabled = false;
+        //tblayout.title = "Kenlinra";
+
+        setSupportActionBar(findViewById(R.id.chat_toolbar));
+        supportActionBar?.apply {
+            title = "Kiung";
+            setDisplayShowHomeEnabled(true);
+            setDisplayHomeAsUpEnabled(true);
+            elevation = 5F;
+        }
     }
 /**
 * *         ONSTART
  * CHECK USER AUTHENTICATION
  *
 */
-    override fun onStart() {
+    override fun onStart()
+    {
         super.onStart()
     }
 
     
     // + --------->>-------->>--------->>*** -->>----------->>>>
-    companion object {
+    companion object
+    {
         val chatIntentkey : String = "CHAT ROOM INFO INTENT KEY";
         val chatJollykey: String = " CHAT JOLLY BUNDLE KEY";
     }
